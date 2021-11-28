d1e4eaf5b8435a606cd68eef4cb8d2b1f2ca1244
Nikola <nikola.r.totev@gmail.com>
2021-11-28 13:51:06 +0200

    Erased old code.

--- Tic_Tac_Toe_CLI/GameState.cs
+++ Tic_Tac_Toe_CLI/GameState.cs
@@ -13,130 +13,31 @@ namespace Tic_Tac_Toe_CLI
         public int alpha;
         public int beta;
         public int value;
-        public Players m_FirstPlayer;
-        public Players winner = Players.NA;
-        public Players currentTurn;
         public int[,] Board = new int[,] { { -1, -1, -1 }, { -1, -1, -1 }, { -1, -1, -1 } };
         public List<Point> possibleMoves = new List<Point>();
         public List<GameState> Children = new List<GameState>();
-        public bool gameOver;
-        public string CharacteristicString;
-
-        public int nextBestMove;
-        public string nextBestMoveCharacteristicString;
-        public GameState nextBestState;
+  
 
 
         public GameState(Players firstPlayer, int[,] board, Players turn)
         {
-            m_FirstPlayer = firstPlayer;
-            currentTurn = turn;
-            Board = board;
-
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    builder.Append(board[i, j].ToString());
-                }
-            }
-
-            CharacteristicString = builder.ToString();
-
-            EvaluateState();
+           
         }
 
         public void EvaluateState()
         {
-            Tuple<Players, bool> winCheck = CheckForWinners();
-            winner = winCheck.Item1;
-            if (winCheck.Item2)
-            {
-                gameOver = true;
-            }
-            value = EvalFunction();
+           
         }
 
         public int EvalFunction()
         {
-            if (winner == Players.Neither)
-            {
-                return 0;
-            }
-
-            if (winner == Players.NA)
-            {
-                return 2;
-            }
-            return (winner == m_FirstPlayer) ? 1 : -1;
+          
 
         }
 
         public Tuple<Players, bool> CheckForWinners()
         {
-            bool hasEqualRow = false;
-
-            //Check Rows
-            for (int i = 0; i < 3; i++)
-            {
-                if (Board[i, 0] == Board[i, 1] && Board[i, 1] == Board[i, 2])
-                {
-                    if (Board[i, 0] != -1)
-                    {
-                        return new Tuple<Players, bool>(GetPlayerType(Board[i, 0]), true);
-                    }
-                }
-            }
-
-            //Check Columns
-            for (int i = 0; i < 3; i++)
-            {
-                if (Board[0, i] == Board[1, i] && Board[1, i] == Board[2, i])
-                {
-                    if (Board[0, i] != -1)
-                    {
-                        return new Tuple<Players, bool>(GetPlayerType(Board[i, 0]), true);
-                    }
-                }
-            }
-
-            //Check Diagonals
-            if (Board[0, 0] == Board[1, 1] && Board[1, 1] == Board[2, 2])
-            {
-                if (Board[0, 0] != -1)
-                {
-                    return new Tuple<Players, bool>(GetPlayerType(Board[0, 0]), true);
-                }
-            }
-
-            if (Board[0, 2] == Board[1, 1] && Board[1, 1] == Board[2, 0])
-            {
-                if (Board[2, 0] != -1)
-                {
-                    return new Tuple<Players, bool>(GetPlayerType(Board[2, 0]), true);
-                }
-            }
-
-            bool boardFull = true;
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    if (Board[i, j] == -1)
-                    {
-                        boardFull = false;
-                        break;
-                    }
-                }
-            }
-
-            if (boardFull)
-            {
-                return new Tuple<Players, bool>(Players.Neither, true);
-            }
-
-            return new Tuple<Players, bool>(Players.Player, false);
+           
         }
 
 
@@ -166,53 +67,19 @@ namespace Tic_Tac_Toe_CLI
 
         public void FindPossibleMoves()
         {
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    if (Board[i, j] == -1)
-                    {
-                        possibleMoves.Add(new Point(i, j));
-                    }
-                }
-            }
-
-            if (possibleMoves.Count == 0)
-            {
-                gameOver = true;
-            }
+           
         }
 
         public GameState CreateChildState(Point move, Players turn)
         {
             int[,] newBoard = new int[3, 3];
             Array.Copy(Board, newBoard, Board.Length);
-            switch (turn)
-            {
-                case Players.Player:
-                    newBoard[move.X, move.Y] = 0;
-                    break;
-                case Players.Computer:
-                    newBoard[move.X, move.Y] = 1;
-                    break;
-            }
-
-            GameState newChild = new GameState(m_FirstPlayer, newBoard, turn);
-            Children.Add(newChild);
-            return newChild;
+            
         }
 
         public void PlaceMove(Point move, Players turn)
         {
-            switch (turn)
-            {
-                case Players.Player:
-                    Board[move.X, move.Y] = 0;
-                    break;
-                case Players.Computer:
-                    Board[move.X, move.Y] = 1;
-                    break;
-            }
+           
         }
     }
 }
--- Tic_Tac_Toe_CLI/TicTacToeAutoPlayer.cs
+++ Tic_Tac_Toe_CLI/TicTacToeAutoPlayer.cs
@@ -16,25 +16,14 @@ namespace Tic_Tac_Toe_CLI
     }
     class TicTacToeAutoPlayer
     {
-        public bool winnerFound;
-
         //-1 = free field, 0 - player, 1 - computer
         public static int[,] board = new int[,] { { -1, -1, -1 }, { -1, -1, -1 }, { -1, -1, -1 } };
         public Point nextTurn;
-        public bool waitingForPlayerToChoose;
-        public int totalTurns = 0;
-        public Players firstPlayer;
-        public Players winner = Players.NA;
-        public GameState currentState = new GameState(Players.Player, board: board, Players.Player);
-        public bool childrenBuilt;
+        
 
         public TicTacToeAutoPlayer(bool computerStartsFirst)
         {
-            firstPlayer = Players.Player;
-            if (computerStartsFirst)
-            {
-                firstPlayer = Players.Computer;
-            }
+           
         }
 
         public void AskForNextTurn()
@@ -48,107 +37,18 @@ namespace Tic_Tac_Toe_CLI
 
         public void Play()
         {
-            while (!currentState.gameOver)
-            {
-                AskForNextTurn();
-                currentState.PlaceMove(nextTurn, Players.Player);
-                if (!childrenBuilt)
-                {
-                    BuildChildren(currentState, Players.Player, totalTurns);
-                }
-                totalTurns++;
-                PrintBoard();
-                Minimax(currentState, 0, 0, 0, Turn.max);
-                currentState = currentState.nextBestState;
-                totalTurns++;
-                PrintBoard();
-            }
+            
         }
 
         public void BuildChildren(GameState state, Players turn, int numberOfTurns)
         {
-            state.FindPossibleMoves();
-            foreach (Point move in state.possibleMoves)
-            {
-                GameState child;
-                Players newTurn;
-                if (turn == Players.Computer)
-                {
-                    newTurn = Players.Player;
-                }
-                else
-                {
-                    newTurn = Players.Computer;
-                }
-
-                child = state.CreateChildState(move, newTurn);
-
-                if (numberOfTurns < 9)
-                {
-                    BuildChildren(child, newTurn, numberOfTurns++);
-                }
-            }
-
-            childrenBuilt = true;
+           
         }
 
 
         public GameState Minimax(GameState state, int numberOfMoves, int alpha, int beta, Turn turn)
         {
-            state.EvaluateState();
-
-            if (state.Children.Count == 0 || state.gameOver)
-            {
-                return state;
-            }
-            GameState result;
-            switch (turn)
-            {
-                case Turn.min:
-                    int minEval = int.MaxValue;
-                    GameState minState = null;
-
-                    //Console.WriteLine($"Rec depth {numberOfMoves}, Char str {state.CharacteristicString}, Child Num {state.Children.Count}");
-                    foreach (GameState child in state.Children)
-                    {
-                        result = Minimax(child, numberOfMoves + 1, alpha, beta, Turn.max);
-                        if (result.value < minEval)
-                        {
-                            minEval = result.value;
-                            minState = result;
-                        }
-                    }
-
-                    state.nextBestMoveCharacteristicString = minState.CharacteristicString;
-                    state.nextBestState = minState;
-                    state.nextBestMove = minState.nextBestMove;
-                    return minState;
-
-                case Turn.max:
-                    int maxEval = int.MinValue;
-                    GameState maxState = null;
-
-                    //Console.WriteLine($"Rec depth {numberOfMoves}, Char str {state.CharacteristicString}, Child Num {state.Children.Count}");
-                    foreach (GameState child in state.Children)
-                    {
-                        result = Minimax(child, numberOfMoves + 1, alpha, beta, Turn.min);
-                        //Console.WriteLine($"Rec depth: {numberOfMoves} Res Val {result.value}");
-                        if (result.value > maxEval)
-                        {
-                            maxEval = result.value;
-                            maxState = result;
-                        }
-
-                        int a = 2;
-                    }
-                    state.nextBestMoveCharacteristicString = maxState.CharacteristicString;
-                    state.nextBestState = maxState;
-                    state.nextBestMove = maxState.nextBestMove;
-                    return maxState;
-
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(turn), turn, null);
-            }
+           
         }
 
 
